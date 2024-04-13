d4a99896248e9a7e132b945f3c16b7f12e9e5a7d
Marc Isaacson <chicago.triathlete@gmail.com>
2024-04-13 17:35:31 -0600

    Build custom subtheme
    This commit:
    * Defines the custom subtheme
    * Sets the custom subtheme as the default
    * Configures the blocks for the subtheme
    * Updates the configuration the the Basic HTML text format

---
+++ web/themes/custom/cffa_claro_subtheme/js/cffa-claro-subtheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * CFFA (Claro Subtheme) behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.cffaClaroSubtheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
